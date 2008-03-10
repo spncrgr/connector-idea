a7f08c78b743e8067f78d0f2e61e588ddfa182ef
jjaroczynski <devnull@localhost>
2008-03-10 12:03:41 +0000

    PL-134 Crucible and Jira poll time added in configuration dialog

--- src/main/java/com/atlassian/theplugin/configuration/JIRAConfigurationBean.java
+++ src/main/java/com/atlassian/theplugin/configuration/JIRAConfigurationBean.java
@@ -4,12 +4,26 @@ package com.atlassian.theplugin.configuration;
  * Bean storing information about Bamboo servers etc.<p>
  * The class serves both as a configuration provider for plugin logic and Bean for persistence.
  */
-public class JIRAConfigurationBean extends AbstractConfigurationBean {
-    public JIRAConfigurationBean() {
+public class JiraConfigurationBean extends AbstractConfigurationBean {
+
+	private int pollTime = 1;
+
+	public JiraConfigurationBean() {
         super();
     }
 
-    public JIRAConfigurationBean(ProductServerConfiguration cfg) {
-        super(cfg);
-    }
+    public JiraConfigurationBean(ProductServerConfiguration cfg) {
+		super(cfg);
+		if (cfg instanceof JiraConfigurationBean) {
+			this.pollTime = ((JiraConfigurationBean) cfg).getPollTime();
+		}
+	}
+
+	public int getPollTime() {
+		return pollTime;
+	}
+
+	public void setPollTime(int pollTime) {
+		this.pollTime = pollTime;
+	}
 }
