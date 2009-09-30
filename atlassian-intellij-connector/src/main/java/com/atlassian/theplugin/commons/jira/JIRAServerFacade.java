7f220d2d5ff3fc487f9c489fa48c95571cca025e
pmaruszak <devnull@localhost>
2009-09-30 11:40:05 +0000

    preparation to move JIRA components to COMMONS

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/JIRAServerFacade.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/JIRAServerFacade.java
@@ -1,91 +1,87 @@
-/**
- * Copyright (C) 2008 Atlassian
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.atlassian.theplugin.commons.jira;
 
-import com.atlassian.theplugin.commons.jira.api.*;
+import com.atlassian.theplugin.commons.ServerType;
+import com.atlassian.theplugin.commons.jira.api.JIRAAction;
+import com.atlassian.theplugin.commons.jira.api.JIRAActionField;
+import com.atlassian.theplugin.commons.jira.api.JIRAAttachment;
+import com.atlassian.theplugin.commons.jira.api.JIRAComment;
+import com.atlassian.theplugin.commons.jira.api.JIRAComponentBean;
+import com.atlassian.theplugin.commons.jira.api.JIRAConstant;
+import com.atlassian.theplugin.commons.jira.api.JIRAIssue;
+import com.atlassian.theplugin.commons.jira.api.JIRAPriorityBean;
+import com.atlassian.theplugin.commons.jira.api.JIRAProject;
+import com.atlassian.theplugin.commons.jira.api.JIRAQueryFragment;
+import com.atlassian.theplugin.commons.jira.api.JIRAResolutionBean;
+import com.atlassian.theplugin.commons.jira.api.JIRAUserBean;
+import com.atlassian.theplugin.commons.jira.api.JIRAVersionBean;
+import com.atlassian.theplugin.commons.jira.api.JiraIssueAdapter;
+import com.atlassian.theplugin.commons.jira.api.JiraUserNotFoundException;
 import com.atlassian.theplugin.commons.jira.api.rss.JIRAException;
 import com.atlassian.theplugin.commons.remoteapi.ProductServerFacade;
+import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Collection;
+import java.util.List;
 
-public interface JIRAServerFacade extends ProductServerFacade {    
-	List<JIRAIssue> getIssues(JiraServerData server, List<JIRAQueryFragment> query,
-			String sort,
-			String sortOrder,
-			int start,
-			int size) throws JIRAException;
+/**
+ * @author pmaruszak
+ * @date Sep 28, 2009
+ */
+public interface JiraServerFacade extends ProductServerFacade {
+    List<JiraIssueAdapter> getIssues(JiraServerData jiraServerData, List<JIRAQueryFragment> query, String sort, String sortOrder, int start, int size) throws JIRAException;
 
-	List<JIRAIssue> getSavedFilterIssues(JiraServerData server,
-			List<JIRAQueryFragment> query,
-			String sort,
-			String sortOrder,
-			int start,
-			int size) throws JIRAException;
+    List<JiraIssueAdapter> getSavedFilterIssues(JiraServerData jiraServerData, List<JIRAQueryFragment> query, String sort, String sortOrder, int start, int size) throws JIRAException;
 
-	List<JIRAProject> getProjects(JiraServerData server) throws JIRAException;
+    List<JIRAProject> getProjects(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAConstant> getStatuses(JiraServerData server) throws JIRAException;
+    List<JIRAConstant> getStatuses(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAConstant> getIssueTypes(JiraServerData server) throws JIRAException;
+    List<JIRAConstant> getIssueTypes(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAConstant> getIssueTypesForProject(JiraServerData server, String project) throws JIRAException;
+    List<JIRAConstant> getIssueTypesForProject(JiraServerData jiraServerData, String project) throws JIRAException;
 
-	List<JIRAConstant> getSubtaskIssueTypes(JiraServerData server) throws JIRAException;
+    List<JIRAConstant> getSubtaskIssueTypes(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAConstant> getSubtaskIssueTypesForProject(JiraServerData server, String project) throws JIRAException;
+    List<JIRAConstant> getSubtaskIssueTypesForProject(JiraServerData jiraServerData, String project) throws JIRAException;
 
-	List<JIRAQueryFragment> getSavedFilters(JiraServerData server) throws JIRAException;
+    List<JIRAQueryFragment> getSavedFilters(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAComponentBean> getComponents(JiraServerData server, String projectKey) throws JIRAException;
+    List<JIRAComponentBean> getComponents(JiraServerData jiraServerData, String projectKey) throws JIRAException;
 
-	List<JIRAVersionBean> getVersions(JiraServerData server, String projectKey) throws JIRAException;
+    List<JIRAVersionBean> getVersions(JiraServerData jiraServerData, String projectKey) throws JIRAException;
 
-	List<JIRAPriorityBean> getPriorities(JiraServerData server) throws JIRAException;
+    List<JIRAPriorityBean> getPriorities(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAResolutionBean> getResolutions(JiraServerData server) throws JIRAException;
+    List<JIRAResolutionBean> getResolutions(JiraServerData jiraServerData) throws JIRAException;
 
-	List<JIRAAction> getAvailableActions(JiraServerData server, JIRAIssue issue) throws JIRAException;
+    List<JIRAAction> getAvailableActions(JiraServerData jiraServerData, JIRAIssue issue) throws JIRAException;
 
-	List<JIRAActionField> getFieldsForAction(JiraServerData server, JIRAIssue issue, JIRAAction action) throws JIRAException;
+    List<JIRAActionField> getFieldsForAction(JiraServerData jiraServerData, JIRAIssue issue, JIRAAction action) throws JIRAException;
 
-	void progressWorkflowAction(JiraServerData server, JIRAIssue issue, JIRAAction action) throws JIRAException;
+    void progressWorkflowAction(JiraServerData jiraServerData, JIRAIssue issue, JIRAAction action) throws JIRAException;
 
-	void progressWorkflowAction(JiraServerData server, JIRAIssue issue,
-			JIRAAction action, List<JIRAActionField> fields) throws JIRAException;
+    void progressWorkflowAction(JiraServerData jiraServerData, JIRAIssue issue, JIRAAction action, List<JIRAActionField> fields) throws JIRAException;
 
-	void addComment(JiraServerData server, String issueKey, String comment) throws JIRAException;
+    void addComment(JiraServerData jiraServerData, String issueKey, String comment) throws JIRAException;
 
-	JIRAIssue createIssue(JiraServerData server, JIRAIssue issue) throws JIRAException;
+    JiraIssueAdapter createIssue(JiraServerData jiraServerData, JIRAIssue issue) throws JIRAException;
 
-	JIRAIssue getIssue(JiraServerData server, String key) throws JIRAException;
+    JiraIssueAdapter getIssue(JiraServerData jiraServerData, String key) throws JIRAException;
 
-	JIRAIssue getIssueDetails(JiraServerData server, JIRAIssue issue) throws JIRAException;
+    JiraIssueAdapter getIssueDetails(JiraServerData jiraServerData, JIRAIssue issue) throws JIRAException;
 
-	void logWork(JiraServerData server, JIRAIssue issue, String timeSpent, Calendar startDate,
-			String comment, boolean updateEstimate, String newEstimate)
-			throws JIRAException;
+    void logWork(JiraServerData jiraServerData, JIRAIssue issue, String timeSpent, Calendar startDate, String comment, boolean updateEstimate, String newEstimate) throws JIRAException;
 
-	void setAssignee(JiraServerData server, JIRAIssue issue, String assignee) throws JIRAException;
+    void setAssignee(JiraServerData jiraServerData, JIRAIssue issue, String assignee) throws JIRAException;
 
-	JIRAUserBean getUser(JiraServerData server, String loginName) throws JIRAException, JiraUserNotFoundException;
+    JIRAUserBean getUser(JiraServerData jiraServerData, String loginName) throws JIRAException, JiraUserNotFoundException;
 
-	List<JIRAComment> getComments(JiraServerData server, JIRAIssue issue) throws JIRAException;
+    List<JIRAComment> getComments(JiraServerData jiraServerData, JIRAIssue issue) throws JIRAException;
 
-    Collection<JIRAAttachment> getIssueAttachements(JiraServerData server, JIRAIssue issue) throws JIRAException;
+    Collection<JIRAAttachment> getIssueAttachements(JiraServerData jiraServerData, JIRAIssue issue) throws JIRAException;
+
+    void testServerConnection(JiraServerData jiraServerData) throws RemoteApiException;
+
+    ServerType getServerType();
 }
